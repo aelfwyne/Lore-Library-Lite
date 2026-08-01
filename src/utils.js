d1df6ef7a8533dfb8dd1c32d1881a616338f5d37
Aelfwyne <aelfwyne@gmail.com>
2026-08-01 18:00:56 -0500

    Attribution Notices

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,16 @@
+/*
+ * Modifications and standalone adaptation Copyright (c) [2026] [aelfwyne @ github].
+ * Contains code extracted and modified from [Doom's Enhancement Suite for SillyTavern] 
+ * Copyright (c) [DangerDaza].
+ *
+ * Portions of the code were used from the original project, heavily modified for the reduced
+ * scope of this project. Full compliance with AGPL-3.0 is maintained, and all source code is 
+ * provided for free.
+ *
+ * Licensed under the GNU Affero General Public License v3.0 (AGPL-3.0).
+ * See the LICENSE file in the project root for full terms.
+ */
+
 export function escapeHtml(str) {
     if (!str) return '';
     return String(str)
